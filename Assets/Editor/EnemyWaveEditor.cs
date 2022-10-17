1e14a701ac27efa409ff9ad6d5921ef8505deec0
KangweiLiao <Clanceyliao@gmail.com>
2022-10-17 14:09:19 -0400

    Inspector bugs fixed: dynamic dropdown, layout adjustment, ...

--- Assets/Editor/EnemyWaveEditor.cs
+++ Assets/Editor/EnemyWaveEditor.cs
@@ -18,8 +18,8 @@ using UnityEditor.Callbacks;
 [CustomEditor(typeof(EnemyWaveManager))]
 public class EnemyWaveEditor : Editor {
 
-    #region  SerializeProperties
-    //set of SerializeProperties in EnemyWaveManager
+    #region  SerializedProperties
+    // set of serialized properties in EnemyWaveManager
     SerializedProperty spawnInterval;
     SerializedProperty enemyPrefabs;
     SerializedProperty enemySpawnPoints;
@@ -31,7 +31,7 @@ public class EnemyWaveEditor : Editor {
     SerializedProperty randomSpawn;
     #endregion
 
-    int _enemyChoiceIndex = 0;
+    int _enemyChoiceIndex = 0; // store dropdown choice index
     bool playerControlGroup = true, typeControlGroup = true; // indicates the expansion of playerControlGroup toggle menu
     List<string> enemyPrefabsNames = new List<string>(); // store enemies' name (type names)
 
@@ -51,32 +51,39 @@ public class EnemyWaveEditor : Editor {
     public override void OnInspectorGUI() {
         serializedObject.Update(); // set serialized object stream based on what is in the actual class
 
+        enemyPrefabsNames.Clear();
+        enemyPrefabsNames.Add("None");
         for (int i = 0; i < enemyPrefabs.arraySize; i++) {
             // init enemies dropdown values (string list)
             if (enemyPrefabs.GetArrayElementAtIndex(i).objectReferenceValue != null) {
+                // update name array
                 enemyPrefabsNames.Add(enemyPrefabs.GetArrayElementAtIndex(i).objectReferenceValue.name);
             }
         }
 
         playerControlGroup = EditorGUILayout.BeginFoldoutHeaderGroup(playerControlGroup, "Player");
-
         if (playerControlGroup) { // fold content (toggle menu)
             EditorGUI.indentLevel++;
             EditorGUILayout.HelpBox("Player controls will be displayed here.", MessageType.Info);
             EditorGUI.indentLevel--;
         }
-
         EditorGUILayout.EndFoldoutHeaderGroup(); // end fold
 
-        EditorGUILayout.Space(10);
-        EditorGUILayout.LabelField("Enemy Type Controls", EditorStyles.boldLabel);
+        EditorGUILayout.Space(15);
 
+        EditorGUI.BeginChangeCheck(); // begin check for property change
+        EditorGUILayout.PropertyField(enemyPrefabs);
+        if (EditorGUI.EndChangeCheck()) { // if changes happened
+            Repaint(); // Repaint inspector if new prefabs added/removed
+        }
+
+        EditorGUILayout.LabelField("Enemy Type Controls", EditorStyles.boldLabel);
         if (enemyPrefabsNames.Count >= 1) {
             // Dropdown menu to select different type of enemies
             _enemyChoiceIndex = EditorGUILayout.Popup(_enemyChoiceIndex, enemyPrefabsNames.ToArray());
-            if (_enemyChoiceIndex != 0) {
+            if (_enemyChoiceIndex != 0) { // if "None" is not selected in dropdown menu
                 typeControlGroup = EditorGUILayout.BeginFoldoutHeaderGroup(typeControlGroup, enemyPrefabsNames[_enemyChoiceIndex]);
-                if (typeControlGroup) {
+                if (typeControlGroup) { // display specific enemy type controls
                     EditorGUI.indentLevel++;
                     EditorGUILayout.HelpBox(string.Format("Specific controls for {0} will be displayed here.",
                         enemyPrefabsNames[_enemyChoiceIndex]), MessageType.Info);
@@ -93,31 +100,31 @@ public class EnemyWaveEditor : Editor {
                     EditorGUILayout.PropertyField(maxSpeed);
                     EditorGUILayout.EndHorizontal();
 
-                    EditorGUIUtility.labelWidth = 110;
+                    EditorGUIUtility.labelWidth = 100;
                     EditorGUILayout.Slider(attackSpeed, 1f, 10f); // use slider to control attack speed
-                    EditorGUILayout.Slider(spawnInterval, 1f, 10f); // use slider to control spawnInterval
                     EditorGUI.indentLevel--;
                 }
                 EditorGUILayout.EndFoldoutHeaderGroup();
             }
         }
-        EditorGUILayout.Space(5); // add a space (5 pixels) in between
 
-        EditorGUI.BeginChangeCheck(); // detect property change
-        EditorGUILayout.PropertyField(enemyPrefabs);
-        if (EditorGUI.EndChangeCheck()) {
-            // if new prefabs added/removed
-            Repaint();
-        }
         EditorGUILayout.Space(5);
-        EditorGUILayout.PropertyField(enemySpawnPoints);
-        EditorGUIUtility.labelWidth = 100;
-        EditorGUILayout.PropertyField(randomSpawn);
-        
-        serializedObject.ApplyModifiedProperties(); // apply changes been made to a SerializedObject
+        EditorGUILayout.PropertyField(enemySpawnPoints); // Spawn points array
+        EditorGUILayout.Space(5);
+        EditorGUILayout.Slider(spawnInterval, 1f, 10f); // use slider to control spawnInterval
+        EditorGUILayout.PropertyField(randomSpawn); // toggle randomSpawn
 
         //if (GUILayout.Button("Open Editor Window")) {
         //    EnemyWaveWindow.ShowWindow(); // Open editor window and pass an EnemyWaveManager
         //}
+
+        serializedObject.ApplyModifiedProperties(); // apply changes been made to the serializedObject
+    }
+
+    void DrawGUILine(float i_height = 1.2f) {
+        // draw a horizontal line in inspector; from https://forum.unity.com/threads/horizontal-line-in-editor-window.520812/
+        Rect rect = EditorGUILayout.GetControlRect(false, i_height);
+        rect.height = i_height;
+        EditorGUI.DrawRect(rect, new Color(0.5f, 0.5f, 0.5f, 1));
     }
 }
