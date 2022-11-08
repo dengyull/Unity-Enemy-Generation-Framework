17fd7248bb93bbf183220c0032ba03a0c04c4d36
KangweiLiao <Clanceyliao@gmail.com>
2022-11-07 23:58:01 -0500

    Bug fixed & delete unnessessary empty lines & comments

--- Assets/Scripts/GEGScoreManager.cs
+++ Assets/Scripts/GEGScoreManager.cs
@@ -1,4 +1,5 @@
 using System;
+using Random = UnityEngine.Random;
 
 namespace GEGFramework {
     /// <summary>
@@ -33,8 +34,11 @@ namespace GEGFramework {
         /// <param name="peakDuration">Desired peak duration</param>
         /// <returns></returns>
         public int GetDifficulty(GEGPackedData data, int zeroDuration, int lowDuration, int peakDuration) {
+
+            int newDiff = prevDiff; // New difficulty level to return
             numRounds++; // Means 10 seconds passed if GEGPackedData.diffEvalInterval = 10f
-            switch(prevDiff) { // update round counters
+
+            switch (prevDiff) { // update round counters
                 case 0:
                     counters.zeroRounds++; // rounds (stay in zero difficulty level) + 1
                     break;
@@ -45,89 +49,51 @@ namespace GEGFramework {
                     counters.peakRounds++; // rounds (stay in 8-10 difficulty level) + 1
                     break;
             }
-            int newDiff = prevDiff; // New difficulty level to return
+
             for (int i = 0; i < GEGPackedData.playerData.Count; ++i) { // For each player do...
                 double currHealth = GEGPackedData.playerData[i].Find("health").value; // Get health property of this player
                 // TODO: Assessing player conditions...
                 // TODO: Perhaps also obtain the targeted player (weaker player) here?
             }
 
-            // TODO: base on players' status evaluate diffLevels:
-
-
             // Cases for difficulty equals to 0
-            if (prevDiff == 0)
-            {
+            if (prevDiff == 0) {
                 // Enough time to relax, so we set difficulty to very low level including 1, 2, 3. 
-                if (counters.zeroRounds > zeroDuration)
-                {
+                if (counters.zeroRounds > zeroDuration) {
                     counters.zeroRounds = 0;
                     newDiff = Random.Range(1, 4);
                 }
-
                 // Else not enough time to relax, so we continue 0 difficulty so do nothing
-            }
-
-
-            // Cases for low difficulty from 1 to 6
-            else if (prevDiff > 0 && prevDiff < 7)
-            {
+            } else if (prevDiff > 0 && prevDiff < 7) { // Cases for low difficulty from 1 to 6
                 // Enough time for low level difficulty, so we switch to high level difficulty.
-                if ((counters.lowRounds > lowDuration)
-                {
+                if (counters.lowRounds > lowDuration) {
                     counters.lowRounds = 0;
                     newDiff = 7;
-                }
-                // Not enough time in low level
-                else
-                {
+                } else { // Not enough time in low level
                     //Not reached the highest level for low difficulty yet, so we make the difficulty a little bit higher
-                    if (prevDiff < 6)
-                    {
+                    if (prevDiff < 6) {
                         newDiff += 1;
                     }
                 }
-
-            }
-
-
-
-            // Cases for high difficulty 7,8,9
-            else if (prevDiff >= 7)
-            {
-                if (counters.peakRounds > peakDuration)
-                {
+            } else if (prevDiff >= 7) { // Cases for high difficulty 7,8,9
+                if (counters.peakRounds > peakDuration) {
                     counters.peakRounds = 0;
                     newDiff = 0;
-                }
-                else
-                {
-                    // 1. The time is not long enough
+                } else { // The peak time is not long enough
                     int continuePeakOrNot = Random.Range(0, 10);
 
-                    // 30% new difficulty = 0
-                    if (continuePeakOrNot >= 7)
-                    {
+                    if (continuePeakOrNot >= 7) { // 30% new difficulty = 0
                         counters.peakRounds = 0;
                         newDiff = 0;
-                    }
-
-                    // 70% increase difficulty
-                    else
-                    {
-                        if (prevDiff < 9)
-                        {
+                    } else { // 70% increase difficulty
+                        if (prevDiff < 9) {
                             newDiff += 1;
                         }
                     }
                 }
             }
-
             prevDiff = newDiff;
             return newDiff;
         }
-
-        
     }
-
 }
