1b68a89f4c91bb37851f68f98fb08353dc66ca60
KangweiLiao <Clanceyliao@gmail.com>
2022-11-21 12:40:01 -0500

    Add default value

--- Assets/Scripts/GEGCharacterProperty.cs
+++ Assets/Scripts/GEGCharacterProperty.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 namespace GEGFramework {
     public abstract class GEGCharacterProperty : ScriptableObject {
 
-        public double value;
+        public double value; // template value to generate the property value in each instance
+        public double defaultValue;
         public string propName;
-        public float diffWeight;
+        public float importance;
         public bool diffEnabled;
 
         /// <summary>
-        /// 
+        /// Default Constructor
         /// </summary>
         /// <param name="propName"></param>
         /// <param name="value"></param>
         /// <param name="enabled"></param>
-        public GEGCharacterProperty(string propName, double value, float weight, bool enabled) {
-            this.propName = propName;
+        public GEGCharacterProperty(string propName, double value, float importance, bool enabled) {
             this.value = value;
-            diffWeight = weight;
+            defaultValue = value;
+            this.propName = propName;
+            this.importance = importance;
             diffEnabled = enabled;
         }
 
-        public override string ToString() => String.Format("{0}:\n\tcurrent value: {1}\n\tdiffWeight:{2}\n\tenabled:{3}.",
-            propName, value, diffWeight, diffEnabled);
+        public override string ToString() => String.Format("{0}:\n\tcurrent value: {1}\n\tdiffWeight:{2}\n\tenabled:{3}",
+            propName, value, importance, diffEnabled);
     }
 }
