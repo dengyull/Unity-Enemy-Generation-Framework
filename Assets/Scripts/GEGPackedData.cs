54e0fd576cbb158497f250aeec70fca6f335b147
KangweiLiao <Clanceyliao@gmail.com>
2022-11-21 16:01:32 -0500

    nextWaveNum added

--- Assets/Scripts/GEGPackedData.cs
+++ Assets/Scripts/GEGPackedData.cs
@@ -5,11 +5,13 @@ namespace GEGFramework {
     /// <summary>
     /// Packed data for GEG Framework.
     /// </summary>
+    [System.Serializable]
     class GEGPackedData {
+        public static bool randomSpawn;
         public static float waveInterval; // Time interval (in seconds) between each wave
         public static float diffEvalInterval; // Time interval (in seconds) between each difficulty score evaluation
+        public static List<Transform> enemySpawnPoints;
         public static List<GEGCharacter> characters; // Data of each type of character (in GEGCharacterInst type)
-        public static Dictionary<string, int> newEnemySpawnNum;
 
         /// <summary>
         /// Default GEGPackedData constructor
@@ -18,7 +20,6 @@ namespace GEGFramework {
             GEGPackedData.waveInterval = waveInterval;
             GEGPackedData.diffEvalInterval = diffEvalInterval;
             characters = new List<GEGCharacter>();
-            newEnemySpawnNum = new Dictionary<string, int>();
         }
 
         public void Test() { // Add test data
