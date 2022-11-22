adc0febfd84095c49e09f4f7cbf3d86a5e86d2c4
dengyull <43188363+dengyull@users.noreply.github.com>
2022-11-22 10:06:38 -0500

    bug fix

--- Assets/_CompletedAssets/Scripts/Enemy/GEGEnemyHealth.cs
+++ Assets/_CompletedAssets/Scripts/Enemy/GEGEnemyHealth.cs
@@ -37,7 +37,7 @@ namespace CompleteProject
         }
         void Start()
         {
-            startingHealth = (int)_character["ZomBearHealth"].defaultValue;
+            startingHealth = (int)_character["ZomBearHealth"].value;
             // Setting the current health when the enemy first spawns.
             currentHealth = startingHealth;
         }
