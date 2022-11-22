760348c9ac6fe4c1e40a2fe978a21f81ce6d849c
dengyull <43188363+dengyull@users.noreply.github.com>
2022-11-22 01:19:47 -0500

    Update GEGDifficultyManager.cs

--- Assets/Scripts/GEGDifficultyManager.cs
+++ Assets/Scripts/GEGDifficultyManager.cs
@@ -3,11 +3,13 @@ using Random = UnityEngine.Random;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace GEGFramework {
+namespace GEGFramework
+{
     /// <summary>
     /// Responsible for computing a new difficulty level (score)
     /// </summary>
-    class GEGDifficultyManager {
+    class GEGDifficultyManager
+    {
 
         int prevDiff; // Previous difficulty level
         int currentRounds;  // Counting rounds for staying at the current difficulty level:
@@ -16,7 +18,8 @@ namespace GEGFramework {
         /// GEGScoreManager constructor
         /// </summary>
         /// <param name="defaultDiff">Initial difficulty level</param>
-        public GEGDifficultyManager(int defaultDiff) {
+        public GEGDifficultyManager(int defaultDiff)
+        {
             if (defaultDiff < 0 || defaultDiff > 10)
                 throw new ArgumentOutOfRangeException("Default difficulty level must be between 0 and 10.");
             currentRounds = 0;
@@ -31,15 +34,18 @@ namespace GEGFramework {
         /// <param name="lowDuration">Desired low difficulty duration</param>
         /// <param name="peakDuration">Desired peak duration</param>
         /// <returns></returns>
-        public int GetDifficulty(GEGPackedData packedData, int zeroDuration, int lowDuration, int peakDuration) {
+        public int GetDifficulty(int zeroDuration, int lowDuration, int peakDuration)
+        {
 
             int newDiff = prevDiff; // New difficulty level to return
             currentRounds++; // Means 10 seconds passed if GEGPackedData.diffEvalInterval = 10f
 
             // Cases for difficulty equals to 0 where no enemy should show up
-            if (prevDiff == 0) {
+            if (prevDiff == 0)
+            {
                 // Enough time to relax, so we set difficulty to very low level including 1, 2, 3. 
-                if (currentRounds > zeroDuration) {
+                if (currentRounds > zeroDuration)
+                {
                     currentRounds = 0;
                     newDiff = Random.Range(1, 4);
                 }
@@ -47,33 +53,46 @@ namespace GEGFramework {
             }
 
             // Cases for low difficulty: from 1 to 6
-            else if (prevDiff > 0 && prevDiff < 7) {
+            else if (prevDiff > 0 && prevDiff < 7)
+            {
                 // Enough time for low level difficulty, so we switch to high level difficulty.
-                if (currentRounds > lowDuration) {
+                if (currentRounds > lowDuration)
+                {
                     currentRounds = 0;
                     newDiff = 7;
-                } else { // Not enough time in low level
+                }
+                else
+                { // Not enough time in low level
                     //Not reached the highest level for low difficulty yet, so we make the difficulty a little bit higher
-                    if (prevDiff < 6) {
+                    if (prevDiff < 6)
+                    {
                         newDiff += 1;
                     }
                 }
             }
 
             // Cases for high difficulty: 7,8,9
-            else if (prevDiff >= 7) {
+            else if (prevDiff >= 7)
+            {
                 // enough peak time, go to relax mode
-                if (currentRounds > peakDuration) {
+                if (currentRounds > peakDuration)
+                {
                     currentRounds = 0;
                     newDiff = 0;
-                } else { // The peak time is not long enough
+                }
+                else
+                { // The peak time is not long enough
                     int continuePeakOrNot = Random.Range(0, 10);
 
-                    if (continuePeakOrNot >= 7) { // 30% new difficulty = 0
+                    if (continuePeakOrNot >= 7)
+                    { // 30% new difficulty = 0
                         currentRounds = 0;
                         newDiff = 0;
-                    } else { // 70% increase difficulty
-                        if (prevDiff < 9) {
+                    }
+                    else
+                    { // 70% increase difficulty
+                        if (prevDiff < 9)
+                        {
                             newDiff += 1;
                         }
                     }
@@ -85,12 +104,27 @@ namespace GEGFramework {
         }
 
         /// <summary>
+        /// previous solution.
+        /// </summary>
+        /// <param name="zeroDuration">Desired zero difficulty duration, counted in diffEval rounds</param>
+        /// <param name="lowDuration">Desired low difficulty duration</param>
+        /// <param name="peakDuration">Desired peak duration</param>
+        /// <returns>Enemy Number.</returns>
+        public void formulaUpdate(int zeroDuration, int lowDuration, int peakDuration)
+        {
+            int Difficulty = GetDifficulty(3, 5, 3);
+            EnemyPropertyGenerator(Difficulty);
+            EnemyNumberUpdate(Difficulty);
+
+        }
+
+        /// <summary>
         /// Generate enemy properties.
         /// </summary>
         /// <param name="difflevel">Difficulty level (from 0 to 10)</param>
         /// <param name="PropertyList">Dictionary contains all attributes with enable or not</param>
         /// <returns></returns>
-        void EnemyPropertyGenerator(int difflevel)
+        public void EnemyPropertyGenerator(int difflevel)
         {
             List<KeyValuePair<string, int>> results = new List<KeyValuePair<string, int>>();
 
@@ -111,12 +145,13 @@ namespace GEGFramework {
             }
         }
 
+
         /// <summary>
         /// Generate enemy number.
         /// </summary>
         /// <param name="difflevel">Difficulty level (from 0 to 10)</param>
         /// <returns></returns>
-        void EnemyNumberGenerator(int difflevel)
+        public void EnemyNumberGenerator(int difflevel)
         {
             List<KeyValuePair<string, float>> temp = new List<KeyValuePair<string, float>>();
             float totalDiffcult = 0;
