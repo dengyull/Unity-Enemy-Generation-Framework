841ca4b49d9bad7aca0e24addf0b0ff8e0dfd97c
KangweiLiao <Clanceyliao@gmail.com>
2022-11-22 10:06:31 -0500

    Bug fixes

--- Assets/Scripts/GEGUIManager.cs
+++ Assets/Scripts/GEGUIManager.cs
@@ -5,20 +5,18 @@ namespace GEGFramework {
         public TMPro.TMP_Text diffifultyText;
         public TMPro.TMP_Text waveText;
 
-        public int waveNum;
-
         // Start is called before the first frame update
         void Start() {
             GEGManager.OnDiffChanged += UpdateDiffText;
-            diffifultyText.text = "Default Difficulty";
-            waveNum = 0;
-            waveText.text = "Wave: "+waveNum;
+            GEGManager.OnNewWaveStart += UpdateWaveText;
         }
 
         // Call this function to update difficulty on UI
         void UpdateDiffText(int currentDiff) {
-            waveNum++;
             diffifultyText.text = "Difficulty Level: " + currentDiff;
+        }
+
+        void UpdateWaveText(int waveNum) {
             waveText.text = "Wave: " + waveNum;
         }
     }
