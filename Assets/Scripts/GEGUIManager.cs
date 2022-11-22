662bc2ddee089909c69f1f1353d6ceb1646be57c
Ezra <73388305+Ezra-fairy@users.noreply.github.com>
2022-11-22 09:42:47 -0500

    Wave Text

--- Assets/Scripts/GEGUIManager.cs
+++ Assets/Scripts/GEGUIManager.cs
@@ -3,17 +3,23 @@ namespace GEGFramework {
     public class GEGUIManager : MonoBehaviour {
         // The Text UI for updating the difficulty level on the screen
         public TMPro.TMP_Text diffifultyText;
+        public TMPro.TMP_Text waveText;
 
+        public int waveNum;
 
         // Start is called before the first frame update
         void Start() {
             GEGManager.OnDiffChanged += UpdateDiffText;
             diffifultyText.text = "Default Difficulty";
+            waveNum = 0;
+            waveText.text = "Wave: "+waveNum;
         }
 
         // Call this function to update difficulty on UI
         void UpdateDiffText(int currentDiff) {
+            waveNum++;
             diffifultyText.text = "Difficulty Level: " + currentDiff;
+            waveText.text = "Wave: " + waveNum;
         }
     }
 }
